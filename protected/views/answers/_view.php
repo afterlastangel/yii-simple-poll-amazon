2f9d019daf25f1805c666b3554bb445a8da4b3d0
Le Kien Truc <afterlastangel@gmail.com>
2014-03-21 22:19:11 +0700

    First commit init project

---
+++ protected/views/answers/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this AnswersController */
+/* @var $data Answers */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('value')); ?>:</b>
+	<?php echo CHtml::encode($data->value); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('question_id')); ?>:</b>
+	<?php echo CHtml::encode($data->question_id); ?>
+	<br />
+
+
+</div>
