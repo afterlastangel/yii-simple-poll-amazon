58ced13e50f2ca86cb8c8fc6b22306ceff4099d0
Le Kien Truc <afterlastangel@gmail.com>
2014-03-23 23:20:23 +0700

    Remove debug

--- protected/views/questions/result.php
+++ protected/views/questions/result.php
@@ -2,9 +2,7 @@
 <?php  
 	foreach ($model->answers as $answer) {
 		$result = 0;
-		error_log(print_r($results,true)."\n",3,'/opt/log/truclk.log');
 		if (isset($results[$answer->id])) {
-			error_log(print_r("aaaaa",true)."\n",3,'/opt/log/truclk.log');
 			$result = $results[$answer->id];
 		}
 		echo "<li id='answer.$answer->id'><span class='result' >$result</span> - $answer->value</li>";
